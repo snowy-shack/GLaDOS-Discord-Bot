63f455bb9e33e65630199e7e6d428b400ab2f164
PhantomEye <armanddevries@outlook.com>
2024-03-30 23:26:26 +0100

    trackBoosters fix

--- src/functions/trackBoosters.js
+++ src/functions/trackBoosters.js
@@ -1,3 +1,17 @@
-boosterRoleID = '852838462469308428'
+async function getBoosters(client) {
 
-message.guild.roles.get(boosterRoleID).members.map(m=>m.user.tag);
+const phGuildID = '704266427577663548';
+const phGuild = client.guilds.cache.get(phGuildID); // Get Phanty's Home server
+
+await phGuild.members.fetch(); // Fetch and cache server members
+
+const boosterRoleID = '852838462469308428';
+const boosterRole = phGuild.roles.cache.get(boosterRoleID); // Get Booster role
+const boosters = boosterRole.members.map(m=>m.user.id); // Get IDs of all Boosters
+
+return boosters;
+}
+
+module.exports = {
+  getBoosters
+};
--- src/index.js
+++ src/index.js
@@ -5,25 +5,20 @@ const fs = require("fs");
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMembers, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent]});
 
-//
-
-//
-
 client.login(process.env.TOKEN);
 
-client.on('messageCreate', (message) => {
+// const ready = require('./events/ready');
+// client.on('ready', ready.execute()); // ???
+
+// Functions
+const trackBoosters = require('./functions/trackBoosters');
+
+
+client.on('messageCreate', async message => {
   if (message.author.id == '382524802491219969' && message.content == 't') {
+    boosters = await trackBoosters.getBoosters(client);
 
-    message.reply('w');
-    message.guild.members.fetch()
-
-
-    phGuild = client.guilds.cache.get('704266427577663548'); // Get Phanty's Home server
-
-    // phGuild.members.fetch();
-    const boosterRoleID = '852838462469308428'
-    
-    console.log(phGuild.roles.cache.get(boosterRoleID).members.map(m=>m.user.tag));
-
+    console.log(boosters.toString());
+    message.reply(boosters.toString());
   }
 });
