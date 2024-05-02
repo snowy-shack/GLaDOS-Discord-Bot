162c539c3c10c03406481e7351565fd476b547ad
PhantomEye <armanddevries@outlook.com>
2024-05-02 02:09:17 +0200

    Finish FAQ implementation

--- src/functions/faqHandler.js
+++ src/functions/faqHandler.js
@@ -1,15 +1,13 @@
-faqs = {
-  portalable_blocks: `
-  # What blocks are portalable in the mod?\nBy default, at least at the moment, the list of blocks you can place portals on includes Endstone, Portalable Panels added by us, and types of Concrete. However, this list is fully configurable by using datapacks to modify the block tag in your world, so if you want any blocks to be portalable as well, that's possible.\n\`id: portalable_blocks\`
-  `
-}
+const faqsJSON = require("../faqs.json");
 
 async function getFaqReply(id) {
-  if (faqs[id]) {
-    return faqs[id];
-  } else {
-    return '❌ Unknown faq ID!';
-  }
+  console.log(id);
+  object = faqsJSON.find(object => object.id == id)
+
+  if (object) {
+    return '# ' + object.question + '\n' + object.answer;
+  } 
+  return '> ❌ `Unknown faq ID!`';
 }
 
 module.exports = { getFaqReply };
