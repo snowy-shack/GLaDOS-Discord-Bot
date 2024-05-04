c403166167ef76acbce629e6bc3343a123fc4387
PhantomEye <armanddevries@outlook.com>
2024-05-05 00:12:29 +0200

    Remove commented code

--- src/index.js
+++ src/index.js
@@ -43,18 +43,6 @@ registerSlashCommands.register();
     if (!message.author.bot && message.guild && reactionChannels.includes(message.channelId)) reactionHandler.react(message);
   });
 
-  // const { EmbedBuilder } = require("discord.js");
-
-  // const testEmbed = new EmbedBuilder().setColor("edaf37")
-	// .setAuthor({ name: "PortalMod donation notification", iconURL: 'https://portalmod.net/images/logo/mark.png' })
-  // .setDescription(
-  //   `**Thank you so much for your donation, \${username}!** We've linked your Portal Gun skins to your Minecraft account.
-    
-  //   You have also been given the **${emojis.supporter}Supporter** role in **Phanty's Home**, which grants you access to <#1005103628882804776>, where you can check out exclusive updates on PortalMod!`
-  // )
-  // .setFooter({text: `Automated message`})
-  // .setTimestamp();
-
   client.on('interactionCreate', async interaction => { // Slash commands
     try {
       if (!interaction.isCommand()) return;
@@ -79,11 +67,3 @@ cron.schedule(
     timezone: "Europe/Amsterdam",
   }
 );
-
-// const start = new SlashCommandBuilder()
-// 	.setName('ban')
-// 	.setDescription('Select a member and ban them.')
-// 	.setDefaultMemberPermissions(PermissionFlagsBits.BanMembers)
-// 	.setDMPermission(false);
-
-// const search = await guild.members.search({ query: 'username' });
