88c7bc0ee66218aed6208562feae1766f9494ffa
PhantomEye <armanddevries@outlook.com>
2024-05-06 12:31:03 +0200

    booster tracking fix

--- src/functions/trackBoosters.js
+++ src/functions/trackBoosters.js
@@ -2,8 +2,8 @@ async function getBoosters(client, phGuild) {
   await phGuild.members.fetch(); // Fetch and cache server members
 
   const boosterRoleID = process.env.BOOSTER_ROLE_ID;
-  const boosterRole = await phGuild.roles.fetch(boosterRoleID); // Get Booster role
-  const boosters = await boosterRole.members.map(m=>m.user.id); // Get IDs of all Boosters
+  const boosterRole = await phGuild?.roles?.fetch(boosterRoleID); // Get Booster role
+  const boosters = await boosterRole?.members.map(m=>m.user.id); // Get IDs of all Boosters
 
   return boosters;
 }
