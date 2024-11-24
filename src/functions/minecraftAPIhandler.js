5becd22300da8ab06d8cb0e6a4fe5f2db25494b6
Phantom <armand.devries@outlook.com>
2024-11-24 15:30:56 +0100

    API update

--- src/functions/minecraftAPIhandler.js
+++ src/functions/minecraftAPIhandler.js
@@ -1,12 +1,12 @@
 async function getUuid(username) {
   const fetch = require('node-fetch');
   
-  const url = `https://api.ashcon.app/mojang/v2/user/${username}`;
+  const url = `https://api.mojang.com/users/profiles/minecraft/${username}`;
   await fetch(url)
     .then(res => res.json())
     .then(json => {
-      uuid = json.uuid;
-      usernameNew = json.username;
+      uuid = `${json.id.slice(0, 8)}-${json.id.slice(8, 12)}-${json.id.slice(12, 16)}-${json.id.slice(16, 20)}-${json.id.slice(20)}`; // Formatting with dashes
+      usernameNew = json.name;
       if (!uuid) return;
     });
   return([await uuid, await usernameNew]);
