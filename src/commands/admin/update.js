b582d18f888f3212001c50e03a3c0deff2e40e38
Phantom <armand.devries@outlook.com>
2025-01-25 20:55:02 +0100

    changed the update exec

--- src/commands/admin/update.js
+++ src/commands/admin/update.js
@@ -13,11 +13,11 @@ function init() {
 
 async function react(interaction) {
   await interaction.reply(logs.formatMessage("⏬ Downloading latest changes"));
-  await logs.logMessage("⏬ Attempting to download latest changes");
+  await logs.logMessage("⏬ Downloading latest changes");
 
   console.log('⏬ Pulling from git');
 
-  exec(path.join(__dirname, '../../../scripts/git-pull.sh'), (error, stdout, stderr) => {
+  exec(`bash ${path.join(__dirname, '../../../scripts/git-pull.sh')}`, (error, stdout, stderr) => {
     console.log("error: " + error);
     console.log("stdout: " + stdout);
     console.log("stderr: " + stderr);
@@ -27,33 +27,32 @@ async function react(interaction) {
       console.error(`exec error: ${error}`);
       logs.logMessage("❌ Update might not have been successful");
       return;
-    }
-    if (stderr) {
+    } else if (stderr) {
       logs.logError(stderr);
       console.error(`stderr: ${stderr}`);
       logs.logMessage("❌ Update might not have been successful");
       return;
-    }
-    
-    setTimeout(async () => {
-      if (stdout.includes("Fast-forward")) {
-        logs.logMessage(`✅ Successfully updated to **GLaDOS v${await getVersion()}**!`);
+    } else {
+      setTimeout(async () => {
+        if (stdout.includes("Fast-forward")) {
+          logs.logMessage(`✅ Successfully updated to **GLaDOS v${await getVersion()}**!`);
 
-      } else if (stdout.includes("Already up to date")) {
-        logs.logMessage(`✅ Already up-to-date: **GLaDOS v${await getVersion()}**`);
-        return;
+        } else if (stdout.includes("Already up to date")) {
+          logs.logMessage(`✅ Already up-to-date: **GLaDOS v${await getVersion()}**`);
+          return;
 
-      } else {
-        logs.logMessage("❌ Update might not have been successful");
-        return;
-      }
-    }, 500);
+        } else {
+          logs.logMessage("⚠️ Update wasn't successful");
+          return;
+        }
+      }, 500);
 
-    // Reboot after 2 seconds
-    setTimeout(async () => {
-      await logs.logMessage("🔁 Rebooting");
-      process.exit();
-    }, 2000);
+      // Reboot after 2 seconds
+      setTimeout(async () => {
+        await logs.logMessage("🔁 Rebooting");
+        process.exit();
+      }, 2000);
+    }    
   });
 }
 
