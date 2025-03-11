27fda8bf81a6d1b82d0911836b34b4d4db7910d7
Phantom <armand.devries@outlook.com>
2025-03-11 19:40:23 +0100

    Upgraded delay functions

--- src/modules/util.js
+++ src/modules/util.js
@@ -1,3 +1,7 @@
-export function wait(time) {
+export function delayInMilliseconds(time) {
     return new Promise(resolve => setTimeout(resolve, time));
+}
+
+export function delayInSeconds(time) {
+    return new Promise(resolve => setTimeout(resolve, time / 1000));
 }
